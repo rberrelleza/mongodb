c3d60d932c95db661fe3585c92112f33f4c9c95d
Ramiro Berrelleza <raberrel@elasticbox.com>
2014-02-17 17:04:39 +0100

    Adding missing curly brace

--- manifests/secondary.pp
+++ manifests/secondary.pp
@@ -7,12 +7,13 @@ define mongodb::secondary(
   $priority = 1,
 ) {
   
-  $replica_add_cmd="result=rs.add(\"${name\"); if (result.ok == 1 || result.errmsg == \"already initialized\"){ \"SUCCESS\"}"
+  $replica_add_cmd="result=rs.add(\"${name}\"); if (result.ok == 1 || result.errmsg == \"already initialized\"){ \"SUCCESS\"}"
   $noauth = "mongo admin --eval '${replica_add_cmd}' | grep SUCCESS -q"
   $auth = "if [ $? -eq 1 ]; then mongo admin -u ${username} -p ${password} --eval '${replica_add_cmd}' | grep SUCCESS -q;fi"
   $command = "${noauth};${auth}"
-  exec { "add_secondary_${name}":
-    command   => $command
+
+  exec { 'add_secondary_${name}':
+    command   => $command,
     path      => "/usr/bin:/usr/sbin:/bin:/sbin",
     logoutput => true,
   }
