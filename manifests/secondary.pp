0b20c960ed682c5bba342e923ad8a150071e70bc
Ramiro Berrelleza <raberrel@elasticbox.com>
2014-02-17 16:32:25 +0100

    Fixing the command to add a secondary as well

--- manifests/secondary.pp
+++ manifests/secondary.pp
@@ -7,8 +7,12 @@ define mongodb::secondary(
   $priority = 1,
 ) {
   
+  $replica_add_cmd="result=rs.add(\"${name\"); if (result.ok == 1 || result.errmsg == \"already initialized\"){ \"SUCCESS\"}"
+  $noauth = "mongo admin --eval '${replica_add_cmd}' | grep SUCCESS -q"
+  $auth = "if [ $? -eq 1 ]; then mongo admin -u ${username} -p ${password} --eval '${replica_add_cmd}' | grep SUCCESS -q;fi"
+  $command = "${noauth};${auth}"
   exec { "add_secondary_${name}":
-    command   => "mongo -u ${username} -p ${password} admin --eval \"rs.add(\\\"${name}\\\")\"",
+    command   => $command
     path      => "/usr/bin:/usr/sbin:/bin:/sbin",
     logoutput => true,
   }
